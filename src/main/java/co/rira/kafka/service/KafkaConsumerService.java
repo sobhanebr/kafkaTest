833c8385af717c91e0456e0bf02418797986991d
Sobhan Ebrahimi <sobhaneb19@yahoo.com>
2020-03-17 01:08:46 +0330

    notification-service-added

--- src/main/java/co/rira/kafka/service/KafkaConsumerService.java
+++ src/main/java/co/rira/kafka/service/KafkaConsumerService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 public class KafkaConsumerService {
 
@@ -21,5 +23,11 @@ public class KafkaConsumerService {
     @KafkaListener(topics = "events", groupId = "kafkaConsumers")
     public void consume(String message) {
         logger.info("#### -> Consumed message -> {}", message);
+        try {
+            notificationService.notifyByEmail(message);
+        } catch (MessagingException e) {
+            logger.error("Error while sending email with content : {}", message, e);
+        }
+        notificationService.notifyBySMS(message);
     }
 }
--- src/main/java/co/rira/kafka/service/NotificationService.java
+++ src/main/java/co/rira/kafka/service/NotificationService.java
@@ -1,8 +1,60 @@
 package co.rira.kafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 @Service
+@EnableAsync
 public class NotificationService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+    private final JavaMailSender mailSender;
+    @Value(value = "${app.email.address}")
+    private String notifiedEmailAddress;
+
+    @Autowired
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Async
+    public void notifyByEmail(String alertText) throws MessagingException {
+        logger.info("Started sending email the message : {}",alertText);
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom("fivegears.rahnema@gmail.com");
+        helper.setTo(notifiedEmailAddress);
+        String text = "<html>\n" +
+                "\n" +
+                "<body dir=\"ltr\">\n" +
+                "    <div>\n" +
+                "            <h3>\n" +
+                alertText+
+                "            </h3>\n" +
+                "    </div>\n" +
+                "\n" +
+                "\n" +
+                "</body>\n" +
+                "\n" +
+                "</html>";
+
+        helper.setText(text, true);
+        helper.setSubject("OpenNMS Alert");
+        mailSender.send(message);
+    }
+
+    public void notifyBySMS(String alertText){
+        logger.info("SMS with content[{}] sent successfully",alertText);
+    }
 
 }
