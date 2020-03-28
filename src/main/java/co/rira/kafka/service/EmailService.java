c6997ca807b6152de44d3d8bfcd0d88659cb8c90
Sobhan Ebrahimi <sobhaneb19@yahoo.com>
2020-03-28 12:03:29 +0430

    email-sending-configuration-updated

---
+++ src/main/java/co/rira/kafka/service/EmailService.java
@@ -0,0 +1,90 @@
+package co.rira.kafka.service;
+
+import co.rira.kafka.model.org.opennms.netmgt.config.notifications.Notification;
+import co.rira.kafka.model.org.opennms.netmgt.config.users.User;
+import co.rira.kafka.utils.notification.NotifierService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.List;
+
+/**
+ * This class manages sending notifications, using SMTP through JavaMailSender
+ */
+@Service
+@EnableAsync
+@Log4j2
+public class EmailService implements NotifierService {
+
+    private final JavaMailSender mailSender;
+    /**
+     * User email address (Can be set in /resources/application.properties)
+     */
+    @Value(value = "${app.email.address}")
+    private String notifiedEmailAddress;
+    @Value(value = "${spring.mail.username}")
+    private String emailSenderAddress;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    /**
+     * Sends email to the user
+     *
+     * @param message email content
+     * @throws MessagingException if cannot send
+     */
+    @Async
+    public void notifyUser(String message) throws MessagingException {
+        log.debug("Started sending email to the : {}", notifiedEmailAddress);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setFrom(emailSenderAddress);
+        helper.setTo(notifiedEmailAddress);
+        String text = "<html>\n" +
+                "\n" +
+                "<body dir=\"ltr\">\n" +
+                "    <div>\n" +
+                "            <h3>\n" +
+                message +
+                "            </h3>\n" +
+                "    </div>\n" +
+                "\n" +
+                "\n" +
+                "</body>\n" +
+                "\n" +
+                "</html>";
+        helper.setText(text, true);
+        helper.setSubject("OpenNMS Alert");
+        mailSender.send(mimeMessage);
+        log.debug("An email was sent to the {} successfully", notifiedEmailAddress);
+    }
+
+    @Override
+    @Async
+    public void notifyUser(User targetUser, Notification notification) throws MessagingException {
+        //todo
+    }
+
+    @Override
+    @Async
+    public void notifyUsers(List<User> notificationTargetList, Notification notification) {
+        notificationTargetList.forEach(user -> {
+            try {
+                notifyUser(user, notification);
+            } catch (MessagingException e) {
+                log.error("Error while sending notification[{}] to user[{}]", notification, user, e);
+            }
+        });
+    }
+}
